283b57abd4434c91d0c688cef687491991b31b2e
Henry Salgado <54928471+henryco94@users.noreply.github.com>
2023-06-20 12:26:33 -0600

    Update dashboard5.py

--- dashboard5.py
+++ dashboard5.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-# Most updated vesion of student survey dashboard
+
 st.title('Science Mill Student Summer Camp Dashboard')
 
 # Load the pre and post survey data
@@ -13,33 +13,20 @@ columns_to_drop = ['#', 'Responder','Person', 'Teacher Number', 'Type', 'Approva
 df_pre = df_pre.drop(columns=columns_to_drop, errors='ignore')
 df_post = df_post.drop(columns=columns_to_drop, errors='ignore')
 
-# Define a mapping from the full responses to the broad categories
-career_mapping = {
-    'General Engineer - General engineers use principles of science and mathematics to solve technical problems in various sectors. They could work in several branches of engineering, such as mechanical, civil, chemical, or electrical, designing and building structures, machines, devices, systems, and processes.': 'General Engineer',
-    'Healthcare Professional - This can encompass a wide variety of career paths, from becoming a physician or nurse, to more specialized roles like medical technologist or radiologist. These professionals use scientific principles to diagnose and treat illnesses, conduct medical research, or operate complex medical equipment.': 'Healthcare Professional',
-    'Software Developer - Software developers design, build, and maintain computer software. They might create everything from operating systems and network controls to mobile apps and cloud-based services.': 'Software Developer',
-    'Environmental Scientist - Environmental scientists use their knowledge of the natural sciences to protect the environment and human health. They may clean up polluted areas, advise policymakers, or work with industry to reduce waste and pollution.': 'Environmental Scientist',
-    'Technical Vocational Professions - This can include a range of hands-on STEM professions that typically require a two-year degree, certification, or apprenticeship, rather than a traditional four-year college degree. This might include electricians, who work with electrical systems and networks; HVAC technicians, who install and repair heating and cooling systems; or automotive technicians, who service and repair vehicles.': 'Technical Vocational Professions'
-}
-
-# Replace the full responses with the broad categories in the post-survey DataFrame
-df_post.replace(career_mapping, inplace=True)
-
 # Get list of all districts
 districts = list(df_pre['District'].unique())
 districts.insert(0, 'All')
 
 # Mapping between pre and post survey questions
 question_mapping = {
-    'When I work in a group, or as part of a team I like ... Sharing my ideas, tasks and responsibilities with the group': 'After participating in this program, do you feel more comfortable sharing your ideas, tasks, and responsibilities when working in a group or as part of a team?',
     'I try new things even if they look hard': 'After coming to this program, I try new things even if they look hard.',
     'I’m good at solving problems': 'After coming to this program, I’m a better problem solver.',
     'I believe I can be successful in a STEM (Science, Technology, Engineering or Math) career': 'I believe I can be successful in a STEM (Science, Technology, Engineering or Math) career.',
     'I think science, technology, engineering or math can be fun': 'I think science, technology, engineering or math can be fun.',
-    #'How much do I think I know about careers in STEM?': 'How much do I think I know about careers in STEM?',
+    'How much do I think I know about careers in STEM?': 'How much do I think I know about careers in STEM?',
     'When I grow up I would be interested in working in a STEM career or job': 'When I grow up I would be interested in working in a STEM career or job.',
-    'I feel like I belong in STEM': 'I feel like I belong in STEM'
 }
+
 # Dropdown to select a district
 selected_district = st.selectbox('Select a district:', districts)
 
@@ -51,13 +38,18 @@ if selected_district != 'All':
 # Dropdown to select the survey type
 survey_type = st.selectbox('Select a survey type:', ['Pre only', 'Post only', 'Mapped'])
 
+# List of all questions in the pre and post survey data
+mapped_questions = list(question_mapping.keys()) + list(question_mapping.values())
+unmapped_questions_pre = [q for q in list(df_pre.columns) if q not in mapped_questions]
+unmapped_questions_post = [q for q in list(df_post.columns) if q not in mapped_questions]
+
 # Determine the list of questions to display based on the selected survey type
 if survey_type == 'Pre only':
-    questions = [q for q in list(df_pre.columns) if q not in question_mapping.keys()]
+    questions = unmapped_questions_pre
 elif survey_type == 'Post only':
-    questions = [q for q in list(df_post.columns) if q not in question_mapping.values()]
+    questions = unmapped_questions_post
 elif survey_type == 'Mapped':
-    questions = list(question_mapping.keys())
+    questions = mapped_questions
 
 # Dropdown to select a question
 selected_question = st.selectbox('Select a question:', questions)
@@ -72,14 +64,22 @@ def add_percentage(ax):
                 ha="center")
 
 # If the selected question is a mapped question
-if selected_question in question_mapping.keys():
+if selected_question in question_mapping.keys() or selected_question in question_mapping.values():
     fig, ax = plt.subplots()
-    post_question = question_mapping[selected_question]
-    pre_counts = df_pre[selected_question].value_counts(normalize=True).reindex(['No', 'Mostly No', 'Mostly Yes', 'Yes'])
-    post_counts = df_post[post_question].value_counts(normalize=True).reindex(['No', 'Mostly No', 'Mostly Yes', 'Yes'])
+    # Identify the pre and post question pair
+    pre_question = [k for k, v in question_mapping.items() if v == selected_question][0] if selected_question in question_mapping.values() else selected_question
+    post_question = question_mapping[pre_question]
+
+    # Count the responses for the pre and post survey questions
+    pre_counts = df_pre[pre_question].value_counts(normalize=True)
+    post_counts = df_post[post_question].value_counts(normalize=True)
+
+    # Create a DataFrame with the response counts for both surveys
     counts = pd.DataFrame({'Pre': pre_counts, 'Post': post_counts})
+
+    # Plot a grouped bar chart with percentages
     counts.plot(kind='bar', color=['blue', 'green'], figsize=(10, 6), ax=ax)
-    plt.title(f'Pre vs Post: {selected_question}')
+    plt.title(f'Pre vs Post: {pre_question}')
     plt.ylabel('Proportion of Responses')
     plt.xticks(rotation=45)
     plt.gca().yaxis.set_major_formatter(plt.matplotlib.ticker.PercentFormatter(1))
@@ -87,32 +87,26 @@ if selected_question in question_mapping.keys():
     plt.tight_layout()
     st.pyplot(fig)
 
+# If the selected question is in the pre survey data, plot a bar chart with percentages
 elif selected_question in df_pre.columns:
     fig, ax = plt.subplots()
-    counts = df_pre[selected_question].value_counts(normalize=True)
-    if len(counts) > 0:
-        counts.plot(kind='bar', color='blue', figsize=(10, 6), ax=ax)
-        plt.title(f'Pre: {selected_question}')
-        plt.ylabel('Proportion of Responses')
-        plt.xticks(rotation=45)
-        plt.gca().yaxis.set_major_formatter(plt.matplotlib.ticker.PercentFormatter(1))
-        add_percentage(ax)
-        plt.tight_layout()
-        st.pyplot(fig)
-    else:
-        st.write("No responses for this question.")
+    df_pre[selected_question].value_counts(normalize=True).plot(kind='bar', color='blue', figsize=(10, 6), ax=ax)
+    plt.title(f'Pre: {selected_question}')
+    plt.ylabel('Proportion of Responses')
+    plt.xticks(rotation=45)
+    plt.gca().yaxis.set_major_formatter(plt.matplotlib.ticker.PercentFormatter(1))
+    add_percentage(ax)
+    plt.tight_layout()
+    st.pyplot(fig)
 
+# If the selected question is in the post survey data, plot a bar chart with percentages
 elif selected_question in df_post.columns:
     fig, ax = plt.subplots()
-    counts = df_post[selected_question].value_counts(normalize=True)
-    if len(counts) > 0:
-        counts.plot(kind='bar', color='green', figsize=(10, 6), ax=ax)
-        plt.title(f'Post: {selected_question}')
-        plt.ylabel('Proportion of Responses')
-        plt.xticks(rotation=45)
-        plt.gca().yaxis.set_major_formatter(plt.matplotlib.ticker.PercentFormatter(1))
-        add_percentage(ax)
-        plt.tight_layout()
-        st.pyplot(fig)
-    else:
-        st.write("No responses for this question.")
+    df_post[selected_question].value_counts(normalize=True).plot(kind='bar', color='green', figsize=(10, 6), ax=ax)
+    plt.title(f'Post: {selected_question}')
+    plt.ylabel('Proportion of Responses')
+    plt.xticks(rotation=45)
+    plt.gca().yaxis.set_major_formatter(plt.matplotlib.ticker.PercentFormatter(1))
+    add_percentage(ax)
+    plt.tight_layout()
+    st.pyplot(fig)
